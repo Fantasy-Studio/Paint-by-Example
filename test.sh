6b2ac1afe7a7890eadb468d4ba5dbef77f0e7ab9
Fantasy-Studio <tennyson@mail.ustc.edu.cn>
2022-11-29 21:12:10 +0800

    .

---
+++ test.sh
@@ -0,0 +1,9 @@
+python scripts/inference.py \
+--plms --outdir tmp/test_data/result \
+--config configs/v1.yaml \
+--ckpt checkpoints/model.ckpt \
+--image_path examples/image/example_1.png \
+--mask_path examples/mask/example_1.png \
+--reference_path examples/reference/example_1.jpg \
+--seed 321 \
+--scale 5
